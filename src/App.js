cd322b1019c9b9fa945ff3b4c7e1f35f3d4ab01b
LienanWisner <lien.argentina@gmail.com>
2023-04-03 19:30:45 -0300

    ultima actualizacion 03/04

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import './App.css';
-import Cards from './components/Cards.jsx';
-import SearchBar from './components/SearchBar.jsx';
+import "./App.css";
+import Cards from "./components/Cards.jsx";
+import SearchBar from "./components/SearchBar.jsx";
 //import characters from './data.js';
-import Nav from './components/Nav';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import Nav from "./components/Nav";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-   
-  let [characters, setCharacters] = useState([]) 
- 
-   //useEffect(onSearch =>{
-     // axios
-   //})
+  let [characters, setCharacters] = useState([]);
 
   function onSearch(id) {
-   axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
-      if (data.name) {
-         setCharacters((oldChars) => [...oldChars, data]);
-      } else {
-         window.alert('¡No hay personajes con este ID!');
-      }
-   });
-}
+    for (const character of characters) {
+      if (character.id === parseInt(id))
+        return window.alert("El personaje ya esta agregado!");
+    }
+    axios(`https://rickandmortyapi.com/api/character/${id}`)
+      .then(({ data }) => {
+        if (data.name) {
+          setCharacters([...characters, data]);
+        } else {
+          window.alert("¡No hay personajes con este ID");
+        }
+      })
+      .catch(() => window.alert("¡No hay personajes con este ID"));
+  }
 
-   const onClose = (id)=>{
-      setCharacters(characters.filter(character=>parseInt(id)!=character.id))
-   }
+  const onClose = (id) => {
+    setCharacters(
+      characters.filter((character) => parseInt(id) != character.id)
+    );
+  };
 
-   return (
-      <div className='App'>
-         <Nav onSearch = {onSearch}/>
-         <Cards onClose={onClose} characters={characters} />
-      </div>
-   );
+  return (
+    <div className="App">
+      <Nav onSearch={onSearch} />
+      <Cards onClose={onClose} characters={characters} />
+    </div>
+  );
 }
 
 export default App;
